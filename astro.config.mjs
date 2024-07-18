6e928d381328f8aa4ccd197d7f9920ddc31fd81c
Yasuhiro Horiuchi <yasuhiro.horiuchi@grikk.co.jp>
2024-07-18 17:34:34 +0900

    google tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,18 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [starlight({
+    site: "",
     title: '猥褻と科学',
+    head: [
+      {tag: 'script',
+        content: `
+w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TKD33BZ8');
+        `}
+    ],
     customCss: [
       // Tailwindのベーススタイルへのパス
       './src/tailwind.css',
